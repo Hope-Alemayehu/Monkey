c0b88b09071c580c7d429575d9e0d060c506bb9f
Hope Alemayehu <hopesp444@gmail.com>
2025-02-07 13:53:18 +0300

    added REPL

---
+++ repl/repl.go
@@ -0,0 +1,36 @@
+/*
+REPL: Read Eval Print Loop
+what it does is REPL reads input, sends it to the interpreter
+for evaluation, prints the result/output of the interpreter and
+starts again. Read Eval Print Loop
+*/
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"monkey/lexer"
+	"monkey/token"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Printf(PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+		line := scanner.Text()
+		l := lexer.New(line)
+
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Printf("%+v\n", tok)
+
+		}
+	}
+}
